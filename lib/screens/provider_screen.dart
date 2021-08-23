e01fa1735c1556b17100b29a70e763e8d2777514
stenlyrachmad <mail.10lee@gmail.com>
2021-08-23 17:07:53 +0800

    Finish Building Section Tersedia Now build Cart Screen

--- lib/screens/provider_screen.dart
+++ lib/screens/provider_screen.dart
@@ -16,6 +16,7 @@ class _ProviderScreenState extends State<ProviderScreen> {
   _buildKendaraanTersedia(Kendaraan kendaraan) {
     return Center(
       child: Stack(
+        alignment: Alignment.center,
         children: [
           Container(
             height: 175.0,
@@ -28,6 +29,49 @@ class _ProviderScreenState extends State<ProviderScreen> {
               borderRadius: BorderRadius.circular(15.0),
             ),
           ),
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black.withOpacity(0.3),
+                  Colors.black87.withOpacity(0.3),
+                  Colors.black54.withOpacity(0.3),
+                  Colors.black38.withOpacity(0.3)
+                ],
+                stops: [0.1, 0.4, 0.6, 0.9],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: [
+                Text(
+                  kendaraan.nama,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'Rp. ${kendaraan.hargaSewa} K',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -136,6 +180,9 @@ class _ProviderScreenState extends State<ProviderScreen> {
               fontSize: 20.0,
               fontWeight: FontWeight.bold,
             ),
+          ),
+          SizedBox(
+            height: 10.0,
           ),
           Expanded(
             child: GridView.count(
